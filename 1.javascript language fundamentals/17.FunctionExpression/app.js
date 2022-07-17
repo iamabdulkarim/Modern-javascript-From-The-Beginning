1afee381008206c81ade63ac5214a79e67d92ada
iamabdulkarim <abdulkarim34.fk@gmail.com>
2022-07-18 00:27:51 +0600

    functions with Imadiately Invokable function and property method

--- 1.javascript language fundamentals/17.FunctionExpression/app.js
+++ 1.javascript language fundamentals/17.FunctionExpression/app.js
@@ -1,5 +1,27 @@
 //Function Declaration
-function greet() {
-  console.log('Hello');
+// function greet1() {
+//   console.log('Hello');
+// }
+// greet();
+function greet(firstName ='john', lastName='Doe') {
+  return 'Hello ' + firstName + ' ' + lastName
 }
-greet();
+console.log(greet());
+
+//Imediately Invokable Function Expressions - IIFEs
+(function (name) {
+  console.log('Hello '+ name)
+})('Abdul karim');
+
+//Property Methods
+
+const todo = {
+  add: function() {
+    console.log('Add todo');
+  },
+  edit: function(id) {
+    console.log(`Edit todo ${id}`);
+  }
+}
+todo.add();
+todo.edit(2);
