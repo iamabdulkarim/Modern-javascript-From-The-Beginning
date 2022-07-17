d553d075da8c87adb82f93b661e4f31ea34752d4
iamabdulkarim <abdulkarim34.fk@gmail.com>
2022-07-17 18:10:53 +0600

    if else logical operator and ternary operator

--- 15.if Else/app.js
+++ 15.if Else/app.js
@@ -4,4 +4,29 @@ if (typeof id !== 'undefined') {
 } else {
   console.log('No Id');
 }
-//result Uncaught ReferenceError: id is not defined
+//result No Id
+
+const color = 'yellow';
+
+if (color === 'red') {
+  console.log('Color is red');  
+} else if(color === 'blue') {
+  console.log('Color is Blue');
+}else{
+  console.log('Color is not red and blue');
+}
+
+//Logical Operators
+const lastName = 'Karim';
+const age = 20;
+if (age > 0 && age <12) {
+  console.log(`${lastName} is a child`);
+}else if(age >= 13 && age <= 19){
+  console.log(`${lastName} is a teenager`);
+} else {
+  console.log(`${lastName} is a adult` );
+}
+
+const number = '100';
+
+console.log(number === 100 ? 'Correct' : 'Incorrect');
